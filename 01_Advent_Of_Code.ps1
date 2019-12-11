f391fa2a37226181eb7aa13710a39ad820e27ac4
Marterich <47688561+Marterich@users.noreply.github.com>
2019-12-11 18:33:33 +0100

    Add files via upload

---
+++ 01_Advent_Of_Code.ps1
@@ -0,0 +1,17 @@
+﻿cls 
+function FuelNeeded ([int]$mass) {
+  
+$fuel = 0;
+
+$fuel = $mass/3
+$fuel = [math]::Truncate($fuel)
+$fuel = $fuel-2
+return $fuel
+}
+$sum = 0;
+foreach ($line in Get-Content "C:\Users\m-wie\OneDrive\Desktop\Advent_Of_Code\01_input.txt")
+{
+$sum += FuelNeeded($line)
+}
+
+Write-Host $sum
